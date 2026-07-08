1fbec1a912ae90cd5e7a3f4870bafa004ac85186
BhakktiGautam <bhakktigautam@gmail.com>
2026-07-08 22:23:42 +0530

    feat: expose new modules in __init__.py
    - Add streaming interceptors to public API
    - Add BFT multi-auditor system to public API
    - Add WebSocket manager to public API
    - Add token stream buffer to public API
    - Make Google imports optional
    - Clean up __all__ list
    
    Closes #246

--- agentwatch/__init__.py
+++ agentwatch/__init__.py
@@ -2,6 +2,7 @@
 
 __version__ = "0.2.0"
 
+# Core
 from agentwatch.core.watcher import (
     AgentWatchBlockedError,
     GenericAdapter,
@@ -10,11 +11,115 @@ from agentwatch.core.watcher import (
     watch,
 )
 
+# Interceptors (Streaming)
+try:
+    from agentwatch.interceptors import (
+        BaseStreamInterceptor,
+        OpenAIStreamInterceptor,
+        AnthropicStreamInterceptor,
+        StreamInterceptorFactory,
+        TokenChunk,
+        TokenStatus,
+        SafetyResult,
+    )
+except ImportError:
+    # Streaming interceptors may not be fully set up yet
+    BaseStreamInterceptor = None
+    OpenAIStreamInterceptor = None
+    AnthropicStreamInterceptor = None
+    StreamInterceptorFactory = None
+    TokenChunk = None
+    TokenStatus = None
+    SafetyResult = None
+
+# Google is optional
+try:
+    from agentwatch.interceptors import GoogleStreamInterceptor
+except ImportError:
+    GoogleStreamInterceptor = None
+
+# Auditors (BFT Multi-Auditor)
+try:
+    from agentwatch.auditors import (
+        BFTAuditor,
+        BaseAuditor,
+        AuditResult,
+        ConsensusEngine,
+        ConsensusResult,
+        ConsensusAlgorithm,
+        TrustScorer,
+        TrustScore,
+        OpenAIAuditor,
+        AnthropicAuditor,
+        GoogleAuditor,
+    )
+except ImportError:
+    BFTAuditor = None
+    BaseAuditor = None
+    AuditResult = None
+    ConsensusEngine = None
+    ConsensusResult = None
+    ConsensusAlgorithm = None
+    TrustScorer = None
+    TrustScore = None
+    OpenAIAuditor = None
+    AnthropicAuditor = None
+    GoogleAuditor = None
+
+# WebSocket
+try:
+    from agentwatch.api.websocket_stream import stream_ws_manager, StreamWebSocketManager
+except ImportError:
+    stream_ws_manager = None
+    StreamWebSocketManager = None
+
+# Token Stream
+try:
+    from agentwatch.core.token_stream import TokenStreamBuffer
+except ImportError:
+    TokenStreamBuffer = None
+
 __all__ = [
+    # Version
     "__version__",
+    
+    # Core
     "watch",
     "detect_framework",
     "detect_framework_label",
     "GenericAdapter",
     "AgentWatchBlockedError",
+    
+    # Streaming Interceptors
+    "BaseStreamInterceptor",
+    "OpenAIStreamInterceptor",
+    "AnthropicStreamInterceptor",
+    "GoogleStreamInterceptor",
+    "StreamInterceptorFactory",
+    "TokenChunk",
+    "TokenStatus",
+    "SafetyResult",
+    
+    # BFT Multi-Auditor
+    "BFTAuditor",
+    "BaseAuditor",
+    "AuditResult",
+    "ConsensusEngine",
+    "ConsensusResult",
+    "ConsensusAlgorithm",
+    "TrustScorer",
+    "TrustScore",
+    "OpenAIAuditor",
+    "AnthropicAuditor",
+    "GoogleAuditor",
+    
+    # WebSocket
+    "stream_ws_manager",
+    "StreamWebSocketManager",
+    
+    # Token Stream
+    "TokenStreamBuffer",
 ]
+
+# Remove None values from __all__
+__all__ = [item for item in __all__ if item is not None]
